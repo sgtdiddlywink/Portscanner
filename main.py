06d7a4469bdbc08eca7806a8d298e6efc7f35a9a
Justin <justinatteberry8@gmail.com>
2023-01-21 00:23:28 -0500

    Script improved with help from ChatGPT.

--- main.py
+++ main.py
@@ -1,45 +1,42 @@
-"""Script to scan ports to see if they are open or closed with the Sockets Library"""
-
 import socket
 import termcolor
+import argparse
+from concurrent.futures import ThreadPoolExecutor
 
 
 def scan(target, ports):
-	for port in range(1, ports + 1):
-		scan_port(target, port)  # Iterate through each of the specified ports to scan
+    with ThreadPoolExecutor() as executor:
+        for port in range(1, ports + 1):
+            executor.submit(scan_port, target, port)
 
 
 def scan_port(ipaddress, port):
-	try:
-		sock = socket.socket()
-		sock.connect((ipaddress, port))
-		print(termcolor.colored(text="[+] Port ({port}) open at address {ipaddress}", color="green"))
-		sock.close()  # Close the socket object once a connection has been established
-	except TimeoutError:
-		print(termcolor.colored(
-			text=f"Timeout Error: Unable to connect to specified address {ipaddress} due to timeout",
-			color="red"
-		))
-		pass
-	except OSError:
-		print(termcolor.colored(
-			text=f"OSError: Unable to connect to specified address {ipaddress} and port {port} due to permissions",
-			color="red"
-		))
-		pass
-	except ConnectionRefusedError:
-		# # Uncomment if you would like to know what ports are closed
-		# print(termcolor.colored(text=f"[-] Port ({port}) closed at address {ipaddress}", color="orange"))
-		pass
+    try:
+        sock = socket.socket()
+        sock.settimeout(5)
+        sock.connect((ipaddress, port))
+        print(termcolor.colored(f"[+] Port ({port}) open at address {ipaddress}", color="green"))
+        sock.close()  # Close the socket object once a connection has been established
+    except TimeoutError:
+        print(termcolor.colored(
+            text=f"Timeout Error: Unable to connect to specified address {ipaddress} due to timeout",
+            color="red"
+        ))
+    except OSError:
+        print(termcolor.colored(
+            text=f"OSError: Unable to connect to specified address {ipaddress} and port {port} due to permissions",
+            color="red"
+        ))
+    except ConnectionRefusedError:
+        # # Uncomment if you would like to know what ports are closed
+        # print(termcolor.colored(text=f"[-] Port ({port}) closed at address {ipaddress}", color="orange"))
+        pass
 
 
-while True:
-	targets = input("[*] Enter target IP Addresses to Scan (Separate targets by a ',' only): ")
-	if " " in targets:
-		print(termcolor.colored(text="Invalid Address(es). Try Again.", color="red"))
-	else:
-		targets_list = targets.split(sep=",")
-		port_range = int(input("[*] Enter how many ports do you want to scan: "))
-		for address in targets_list:
-			scan(target=address, ports=port_range)
-	print(termcolor.colored(text="[+] Scan Complete", color="green"))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Scan open ports")
+    parser.add_argument("target", help="IP address to scan")
+    parser.add_argument("ports", type=int, help="Number of ports to scan")
+    args = parser.parse_args()
+    scan(args.target, args.ports)
+    print(termcolor.colored(text="[+] Scan Complete", color="green"))
